fedbcd1e83ca9e60cc945978b9863b375dd01f1f
yejinhua <yejinhua@gzis.ac.cn>
2024-01-12 15:13:27 +0800

    add：添加创建产品NFT的DTO

---
+++ src/main/java/cn/org/gry/chainmaker/domain/dto/PackagedProductInfoDTO.java
@@ -0,0 +1,26 @@
+package cn.org.gry.chainmaker.domain.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigInteger;
+import java.util.List;
+
+/**
+ * @author yejinhua  Email:yejinhua@gzis.ac.cn
+ * @version 1.0
+ * @description
+ * @since 2024/1/11 16:27
+ * Copyright (C) 2022-2023 CASEEDER, All Rights Reserved.
+ * 注意：本内容仅限于内部传阅，禁止外泄以及用于其他的商业目的
+ */
+@Getter
+@Setter
+public class PackagedProductInfoDTO {
+    private BigInteger numberOfTokens;
+    private String tokenURI;
+    private String name;
+    private String productLot;
+    private List<BigInteger> childIDs;
+    private List<String> resumes;
+}
